140961af48d8c373ed71bf404c1becdc3d9b48d4
Wenchao Liu <wenchaoliu2011@gmail.com>
2023-07-21 22:27:21 +0800

    .

--- simulation.dart
+++ simulation.dart
@@ -2,9 +2,9 @@ import 'dart:math';
 
 void main() {
   print('start');
-  const potential = 10;
-  var population = <double>[0.8, 0.9, 0.9, 1, 1, 1, 1, 1, 1, 1, 1, 1.1, 1.1];
-  for (var i = 1; i <= 2000; i++) {
+  const potential = 25;
+  var population = <double>[0.7, 0.8, 0.9, 0.9, 1, 1, 1, 1, 1.1, 1.1, 1.2, 1.2];
+  for (var i = 1; i <= 1000; i++) {
     final offsprings = <double>[];
     while (population.length >= 2) {
       final last = population.removeLast();
@@ -12,7 +12,7 @@ void main() {
       if (population.length > 10) {
         // sexual selection
         randomIndex =
-            population.length - Random().nextInt(population.length ~/ 8) - 1;
+            population.length - Random().nextInt(population.length ~/ 10) - 1;
       } else {
         randomIndex = population.length - 1;
       }
@@ -25,12 +25,12 @@ void main() {
         final luck = Random().nextDouble();
         var offspring = value;
         // mutation - 60% chance of bad mutation
-        if (luck > 0.9999) {
-          offspring += 0.01;
-        } else if (luck > 0.60) {
+        if (luck > 0.99) {
+          offspring += 0.001;
+        } else if (luck > 0.6) {
           // no mutation
         } else if (luck > 0.5) {
-          offspring -= 0.02;
+          offspring -= 0.025;
         } else if (luck > 0.4) {
           offspring -= 0.05;
         } else if (luck > 0.3) {
@@ -38,15 +38,16 @@ void main() {
         } else if (luck > 0.2) {
           offspring -= 0.2;
         } else if (luck > 0.1) {
-          offspring -= 0.5;
+          offspring -= 0.4;
         } else {
-          offspring = 0;
+          offspring = 0.8;
         }
 
         // survivability - hash environment and competition
-        double competition = offsprings.length / 1000;
+        double competition =
+            offsprings.length / 1000 + population.length / 10000;
 
-        if (offspring > Random().nextDouble() * (1 + competition)) {
+        if (offspring > Random().nextDouble() * (2 + competition)) {
           offsprings.add(offspring);
         }
       }
